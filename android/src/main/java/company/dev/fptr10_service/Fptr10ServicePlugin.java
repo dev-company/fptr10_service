b1104e43435174079b509db3a3ddedfb1709eecc
Mike Kosulin <mk@dev.company>
2023-02-19 15:25:34 +0300

    upd java plugin

--- android/src/main/java/company/dev/fptr10_service/Fptr10ServicePlugin.java
+++ android/src/main/java/company/dev/fptr10_service/Fptr10ServicePlugin.java
@@ -60,7 +60,7 @@ public class Fptr10ServicePlugin implements  MethodCallHandler, FlutterPlugin {
         onGetDriverVersion(call, result);
         break;
       case "getSettings":
-        result.success(fptr.getSettings());
+        onGetSettings(call, result);
         break;
       case "getStatus":
         onGetStatus(call, result);
@@ -113,61 +113,68 @@ public class Fptr10ServicePlugin implements  MethodCallHandler, FlutterPlugin {
   private  void onValidateJson(MethodCall call, Result result) {
     fptr.setParam(IFptr.LIBFPTR_PARAM_JSON_DATA, call.argument("task").toString());
 
-    int responseCode =  fptr.validateJson();
-    if(responseCode != 0) {
+    int responseCode = fptr.validateJson();
+    if (responseCode != 0) {
       result.error(String.format("%s",responseCode), fptr.errorDescription(), null);
-    }else{
+    } else {
       result.success(true);
     }
   }
 
-
-  private  void onGetSettings(MethodCall call, Result result) {
-    String settings =fptr.getSettings();
-
-    if(settings != null){
-      result.success(settings);
-    }else{
-      result.error("SETTINGS_GET_ERROR", fptr.errorDescription(), null);
+  private void onGetSettings(MethodCall call, Result result) {
+    String settings = fptr.getSettings();
+    if (settings == null) {
+     result.error(SETTINGS_GET_ERROR, fptr.errorDescription(), null);
+     return;
     }
+
+    result.success(settings);
   }
 
-  private  void onSetSettings(MethodCall call, Result result) {
-    String settings;
-    settings = String.format("{\"%s\": %d, \"%s\": %d, \"%s\": \"%s\", \"%s\": \"%s\", \"%s\": %d}",
-            IFptr.LIBFPTR_SETTING_MODEL, IFptr.LIBFPTR_MODEL_ATOL_AUTO,
-            IFptr.LIBFPTR_SETTING_PORT, IFptr.LIBFPTR_PORT_TCPIP,
-            IFptr.LIBFPTR_SETTING_COM_FILE, "COM5",
-            IFptr.LIBFPTR_SETTING_IPADDRESS, call.argument("tcp_ip_address"),
-            IFptr.LIBFPTR_SETTING_IPPORT, call.argument("tcp_ip_port"));
+
+  private void onSetSettings(MethodCall call, Result result) {
+    int model = IFptr.LIBFPTR_MODEL_ATOL_AUTO;
+    int port = IFptr.LIBFPTR_PORT_TCPIP;
+    String comFile = "COM5";
+    String tcpIpAddress = call.argument("tcp_ip_address");
+    int tcpIpPort = call.argument("tcp_ip_port");
+
+    String settings = String.format(
+      "{\"%s\": %d, \"%s\": %d, \"%s\": \"%s\", \"%s\": \"%s\", \"%s\": %d}",
+      IFptr.LIBFPTR_SETTING_MODEL, model,
+      IFptr.LIBFPTR_SETTING_PORT, port,
+      IFptr.LIBFPTR_SETTING_COM_FILE, comFile,
+      IFptr.LIBFPTR_SETTING_IPADDRESS, tcpIpAddress,
+      IFptr.LIBFPTR_SETTING_IPPORT, tcpIpPort
+    );
 
     int responseCode = fptr.setSettings(settings);
-    if(responseCode != 0) {
-      result.error(String.format("%s",responseCode), fptr.errorDescription(), null);
-    }else{
+    if (responseCode != 0) {
+      result.error(String.valueOf(responseCode), fptr.errorDescription(), null);
+    } else {
       result.success(true);
     }
-
   }
 
-  private  void onPerformJson(MethodCall call, Result result) {
-    fptr.setParam(IFptr.LIBFPTR_PARAM_JSON_DATA, call.argument("task").toString());
+  private void onPerformJson(MethodCall call, Result result) {
+    String task = call.argument("task").toString();
+    fptr.setParam(IFptr.LIBFPTR_PARAM_JSON_DATA, task);
     int responseCode = fptr.processJson();
+  
     if(responseCode != 0) {
       result.error(String.format("%s",responseCode), fptr.errorDescription(), null);
-    }else{
+    } else {
       String jsonData = fptr.getParamString(IFptr.LIBFPTR_PARAM_JSON_DATA);
       result.success(jsonData);
     }
   }
-
 
   private  void onGetDriverVersion(MethodCall call, Result result){
     String version = fptr.version();
 
     if(version != null){
       result.success(version);
-    }else{
+    } else {
       result.error("VERSION_GET_ERROR", fptr.errorDescription(), null);
     }
   }
@@ -176,7 +183,7 @@ public class Fptr10ServicePlugin implements  MethodCallHandler, FlutterPlugin {
     int responseCode = fptr.close();
     if(responseCode == 0){
       result.success(true);
-    }else{
+    } else {
       result.error(String.format("%s",responseCode), fptr.errorDescription(),null);
     }
   }
@@ -185,84 +192,92 @@ public class Fptr10ServicePlugin implements  MethodCallHandler, FlutterPlugin {
     int responseCode = fptr.open();
     if(responseCode == 0){
       result.success(true);
-    }else{
+    } else {
       result.error(String.format("%s",responseCode), fptr.errorDescription(), null);
     }
   }
 
+
   private void onGetStatus(MethodCall call, Result result) {
-    Map<String, Object>  status = new HashMap<>();
+    Map<String, Object> status = new HashMap<>();
 
-    fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_STATUS);
-    fptr.queryData();
-
-    long operatorID      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_OPERATOR_ID);
-    long logicalNumber   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_LOGICAL_NUMBER);
-    long shiftState      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SHIFT_STATE);
-
-    String modelName     = fptr.getParamString(IFptr.LIBFPTR_PARAM_MODEL_NAME);
-    long mode            = fptr.getParamInt(IFptr.LIBFPTR_PARAM_MODE);
-    long submode         = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SUBMODE);
-    long receiptNumber   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_NUMBER);
-    long documentNumber  = fptr.getParamInt(IFptr.LIBFPTR_PARAM_DOCUMENT_NUMBER);
-    long shiftNumber     = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SHIFT_NUMBER);
-    long receiptType     = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_TYPE);
-    long documentType    = fptr.getParamInt(IFptr.LIBFPTR_PARAM_DOCUMENT_TYPE);
-    long lineLength      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_LINE_LENGTH);
-    long lineLengthPix   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_LINE_LENGTH_PIX);
-
-    double receiptSum = fptr.getParamDouble(IFptr.LIBFPTR_PARAM_RECEIPT_SUM);
-
-    boolean isFiscalDevice          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FISCAL);
-    boolean isFiscalFN              = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FN_FISCAL);
-    boolean isFNPresent             = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FN_PRESENT);
-    boolean isInvalidFN             = fptr.getParamBool(IFptr.LIBFPTR_PARAM_INVALID_FN);
-    boolean isCashDrawerOpened      = fptr.getParamBool(IFptr.LIBFPTR_PARAM_CASHDRAWER_OPENED);
-    boolean isPaperPresent          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_RECEIPT_PAPER_PRESENT);
-    boolean isPaperNearEnd          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PAPER_NEAR_END);
-    boolean isCoverOpened           = fptr.getParamBool(IFptr.LIBFPTR_PARAM_COVER_OPENED);
-    boolean isPrinterConnectionLost = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_CONNECTION_LOST);
-    boolean isPrinterError          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_ERROR);
-    boolean isCutError              = fptr.getParamBool(IFptr.LIBFPTR_PARAM_CUT_ERROR);
-    boolean isPrinterOverheat       = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_OVERHEAT);
-    boolean isDeviceBlocked         = fptr.getParamBool(IFptr.LIBFPTR_PARAM_BLOCKED);
-
-    fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_SHIFT_STATE);
-    fptr.queryData();
+    try {
+      fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_STATUS);
+      fptr.queryData();
 
 
-    status.put("operatorID", operatorID);
-    status.put("logicalNumber", logicalNumber);
-    status.put("shiftState", shiftState);
-    status.put("modelName", modelName);
-    status.put("mode", mode);
-    status.put("submode", submode);
-    status.put("receiptNumber", receiptNumber);
-    status.put("documentNumber", documentNumber);
-    status.put("shiftNumber", shiftNumber);
+      long operatorID      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_OPERATOR_ID);
+      long logicalNumber   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_LOGICAL_NUMBER);
+      long shiftState      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SHIFT_STATE);
 
-    status.put("receiptType", receiptType);
-    status.put("documentType", documentType);
-    status.put("lineLength", lineLength);
-    status.put("lineLengthPix", lineLengthPix);
-    status.put("receiptSum", receiptSum);
-    status.put("isFiscalDevice", isFiscalDevice);
-    status.put("isFiscalFN", isFiscalFN);
-    status.put("isFNPresent", isFNPresent);
-    status.put("isInvalidFN", isInvalidFN);
+      String modelName     = fptr.getParamString(IFptr.LIBFPTR_PARAM_MODEL_NAME);
+      long mode            = fptr.getParamInt(IFptr.LIBFPTR_PARAM_MODE);
+      long submode         = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SUBMODE);
+      long receiptNumber   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_NUMBER);
+      long documentNumber  = fptr.getParamInt(IFptr.LIBFPTR_PARAM_DOCUMENT_NUMBER);
+      long shiftNumber     = fptr.getParamInt(IFptr.LIBFPTR_PARAM_SHIFT_NUMBER);
+      long receiptType     = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_TYPE);
+      long documentType    = fptr.getParamInt(IFptr.LIBFPTR_PARAM_DOCUMENT_TYPE);
+      long lineLength      = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_LINE_LENGTH);
+      long lineLengthPix   = fptr.getParamInt(IFptr.LIBFPTR_PARAM_RECEIPT_LINE_LENGTH_PIX);
 
-    status.put("isCashDrawerOpened", isCashDrawerOpened);
-    status.put("isPaperPresent", isPaperPresent);
-    status.put("isPaperNearEnd", isPaperNearEnd);
-    status.put("isCoverOpened", isCoverOpened);
-    status.put("isPrinterConnectionLost", isPrinterConnectionLost);
-    status.put("isPrinterError", isPrinterError);
-    status.put("isCutError", isCutError);
-    status.put("isPrinterOverheat", isPrinterOverheat);
-    status.put("isDeviceBlocked", isDeviceBlocked);
+      double receiptSum = fptr.getParamDouble(IFptr.LIBFPTR_PARAM_RECEIPT_SUM);
+
+      boolean isFiscalDevice          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FISCAL);
+      boolean isFiscalFN              = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FN_FISCAL);
+      boolean isFNPresent             = fptr.getParamBool(IFptr.LIBFPTR_PARAM_FN_PRESENT);
+      boolean isInvalidFN             = fptr.getParamBool(IFptr.LIBFPTR_PARAM_INVALID_FN);
+      boolean isCashDrawerOpened      = fptr.getParamBool(IFptr.LIBFPTR_PARAM_CASHDRAWER_OPENED);
+      boolean isPaperPresent          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_RECEIPT_PAPER_PRESENT);
+      boolean isPaperNearEnd          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PAPER_NEAR_END);
+      boolean isCoverOpened           = fptr.getParamBool(IFptr.LIBFPTR_PARAM_COVER_OPENED);
+      boolean isPrinterConnectionLost = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_CONNECTION_LOST);
+      boolean isPrinterError          = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_ERROR);
+      boolean isCutError              = fptr.getParamBool(IFptr.LIBFPTR_PARAM_CUT_ERROR);
+      boolean isPrinterOverheat       = fptr.getParamBool(IFptr.LIBFPTR_PARAM_PRINTER_OVERHEAT);
+      boolean isDeviceBlocked         = fptr.getParamBool(IFptr.LIBFPTR_PARAM_BLOCKED);
+
+      fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_SHIFT_STATE);
+      fptr.queryData();
+
+
+      status.put("operatorID", operatorID);
+      status.put("logicalNumber", logicalNumber);
+      status.put("shiftState", shiftState);
+      status.put("modelName", modelName);
+      status.put("mode", mode);
+      status.put("submode", submode);
+      status.put("receiptNumber", receiptNumber);
+      status.put("documentNumber", documentNumber);
+      status.put("shiftNumber", shiftNumber);
+
+      status.put("receiptType", receiptType);
+      status.put("documentType", documentType);
+      status.put("lineLength", lineLength);
+      status.put("lineLengthPix", lineLengthPix);
+      status.put("receiptSum", receiptSum);
+      status.put("isFiscalDevice", isFiscalDevice);
+      status.put("isFiscalFN", isFiscalFN);
+      status.put("isFNPresent", isFNPresent);
+      status.put("isInvalidFN", isInvalidFN);
+
+      status.put("isCashDrawerOpened", isCashDrawerOpened);
+      status.put("isPaperPresent", isPaperPresent);
+      status.put("isPaperNearEnd", isPaperNearEnd);
+      status.put("isCoverOpened", isCoverOpened);
+      status.put("isPrinterConnectionLost", isPrinterConnectionLost);
+      status.put("isPrinterError", isPrinterError);
+      status.put("isCutError", isCutError);
+      status.put("isPrinterOverheat", isPrinterOverheat);
+      status.put("isDeviceBlocked", isDeviceBlocked);
+
+    } catch (Exception e) {
+      result.error("STATUS_ERROR", fptr.errorDescription(), null);
+    }
+
     result.success(status);
-
   }
+
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
